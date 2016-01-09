1b0514044a8c8301f36cc56f94acc42b3350afb9
hezedu <hezedu@hotmail.com>
2016-01-09 16:33:54 +0800

    0.0.1

---
+++ index.js
@@ -0,0 +1,50 @@
+var WXBizMsgCrypt = require('wechat-crypto');
+//钉钉文档：http://ddtalk.github.io/dingTalkDoc/?spm=a3140.7785475.0.0.p5bAUd#2-回调接口（分为五个回调类型）
+module.exports = function(config, callback) {
+  //suite4xxxxxxxxxxxxxxx 是钉钉默认测试suiteid 
+  var newCrypt = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.suiteid || 'suite4xxxxxxxxxxxxxxx');
+
+  return function(req, res, next) {
+
+    var signature = req.query.signature;
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+    var encrypt = req.body.encrypt;
+
+    if (signature !== newCrypt.getSignature(timestamp, nonce, encrypt)) {
+      res.writeHead(401);
+      res.end('Invalid signature');
+      return;
+    }
+
+    var result = newCrypt.decrypt(encrypt);
+    var message = JSON.parse(result.message);
+
+    if (message.EventType === 'check_update_suite_url') { //创建套件第一步，验证有效性。
+      var Random = message.Random;
+      result = _jsonWrapper(timestamp, nonce, Random);
+      res.json(result);
+
+    } else {
+      res.reply = function() {
+        result = _jsonWrapper(timestamp, nonce, 'success');
+        res.json(result);
+      }
+
+      callback(message, req, res, next);
+    };
+  }
+
+
+  function _jsonWrapper(timestamp, nonce, text) {
+    var encrypt = newCrypt.encrypt(text);
+    var msg_signature = newCrypt.getSignature(timestamp, nonce, encrypt);//新签名
+    return {
+      msg_signature: msg_signature,
+      encrypt: encrypt,
+      timeStamp: timestamp,
+      nonce: nonce
+    };
+  }
+
+}
